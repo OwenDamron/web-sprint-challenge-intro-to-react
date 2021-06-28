07f99ae891434680bfaf67ca93ba3917898069e4
Owen  Damron <damronowen@yahoo.com>
2021-06-27 20:26:35 -0500

    Finished Assignment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import styled from 'styled-components'
+import Characters from './components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+const [charName, setCharName] = useState([])
+const [charId, setCharId] = useState(null)
+
+const openChar = id => {
+  setCharId(id)
+}
+
+const closeChar = () => {
+  setCharId(null)
+}
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(({data}) => setCharName(data))
+    .catch((err) => console.log(err))
+  }, [])
+
+  const Char = props => (
+    <div className='character'>
+      {props.info.name}
+      <button onClick={() => openChar(props.info.name)}>
+        Open File
+      </button>
     </div>
+  )
+
+  return (
+    <StyledDiv1 className="App">
+      <h1 className="Header">Wanted</h1><br />
+
+      <StyledDiv2 className="characters">
+        {charName.map(char => {
+          return <Char key={char.name} info={char}/>
+        })}
+        {charId && <Characters name={charId} close={closeChar} />}
+
+      </StyledDiv2>
+    </StyledDiv1>
   );
 }
+
+const StyledDiv1 = styled.div`
+display:flex;
+justify-content:center;`
+
+const StyledDiv2 = styled.div`
+display:flex;
+justify-content:center;
+flex-direction:column;`
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
-// Write your Character component here
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+export default function Characters(props) {
+    const {name, close} = props
+    const [charInfo, setCharInfo] = useState(null)
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/`)
+        .then(({data}) => setCharInfo(data))
+        .catch(err => console.log(err))
+    }, [name])
+
+    return (
+        <div className='charInfo'>
+            <h2>Bounty Info</h2>
+            {
+                charInfo &&
+                <>
+                <ul>
+                    {charInfo.map(char => <li key={char.name}>
+                        {char.name}<br />
+                        Height: {char.height}<br />
+                        Gender: {char.gender}<br />
+                        Mass: {char.mass}<br />
+                        Hair Color: {char.hair_color}<br />
+                        Skin Color: {char.skin_color}<br />
+                        Eye Color: {char.eye_color}<br />
+                        Birth Year: {char.birth_year}<br />
+                    </li>)}
+                </ul>
+                </>
+            }
+            <button onClick={close}>Close File</button>
+        </div>
+    )
+}
